fa9742197ce2bd4900fad232eb6ba757da7c412b
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-29 19:10:51 +0900

    Fix typos and improve book listing logic

--- src/Library.java
+++ src/Library.java
@@ -7,20 +7,19 @@ class Library{
     books.add(book);
   }
   void showBooks(){
-    if(Books.size()==0){
-      System.out.println("登録されている書籍はありません")
+    if(books.size()==0){
+      System.out.println("登録されている書籍はありません");
         }else{
       for(int i=0;i<books.size();i++){
         Book b=books.get(i);
-        System.out.println((i+1)+":"+b.tiile+"/"+b.author+"/"+b.year);
+        System.out.println((i+1)+":"+b.title+"/"+b.author+"/"+b.year);
       }
     }
   }
   boolean removeBook(int number){
     int index=number-1;
     
-    if(number<1||number>
-       books.size()){
+    if(number<1||number>books.size()){
     return false;
   }
   books.remove(index);
