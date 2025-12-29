944fc723269109b5056bcaf6ef42f819c70538bf
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-29 20:29:35 +0900

    Change Book class to have a constructor
    Fix constructor definition and access modifier.

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,14 @@
-public class Book{
+class Book{
   String title;
   String author;
   int year;
+
+  BooK(String title, String author, int year){
+this.title=title;
+this author = author;
+this.year=year;
+  }
 }
+
 
 
