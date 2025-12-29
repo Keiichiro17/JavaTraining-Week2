23dc42be741026156946e29f452320129cd05932
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-29 21:28:07 +0900

    Fix constructor name from 'BooK' to 'Book'

--- src/Book.java
+++ src/Book.java
@@ -3,7 +3,7 @@ class Book{
   String author;
   int year;
 
-  BooK(String title, String author, int year){
+  Book(String title, String author, int year){
 this.title=title;
 this.author = author;
 this.year=year;
