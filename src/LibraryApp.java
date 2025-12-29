e7898a8bbd8a168432cfb7da6dcbcf4a607c8b26
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-29 13:47:16 +0900

    Implement main method to display book title
    Added a simple main method to LibraryApp that creates a Book instance and prints its title.

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,1 +1,11 @@
+public class LibraryApp{
+  public static void main(String[] args)
+  {
+    Book b = new Book();
+    b.title="Java入門";
+    b.author="山田";
+    b.year=2023;
+    System.out.println(b.title);
+  }
+}
 
