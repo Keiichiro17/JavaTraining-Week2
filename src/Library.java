3bab9f4f4176bfb127553754ee307a9800eeab3b
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-29 16:36:23 +0900

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,1 +1,30 @@
+import java.util.ArrayList;
 
+class Library{
+  ArrayList <Book> books = new ArrayList<>();
+  
+  void addBook(Book book){
+// TODO: booksにbookを追加する
+  }
+  void showBooks(){
+    //To do:
+    //1)0件なら「登録されている書籍はありません」みたいに表示
+    //2)1件以上なら、番号付きで一覧表示（1番から）
+    //例："1:タイトル/著者/出版年"
+  }
+  boolean removeBook(int number){
+    //To do:
+    //1)numberをindex=number-1に変換
+    //2)範囲チェック(1未満or件数より大きい→false)
+    //3)削除できたらtrue
+    return false;
+  }
+
+  int size(){
+    //To do:登録冊数を返す
+    return 0;
+  }
+  }
+  
+
+  
