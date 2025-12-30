a381424cfda84d979031428575edcf299440dfe7
Mike <244047558+Keiichiro17@users.noreply.github.com>
2025-12-30 19:58:51 +0900

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,13 @@
+//本一冊の情報を表すクラス
 class Book{
-  String title;
-  String author;
-  int year;
+  String title; //本のタイトル
+  String author; //著者
+  int year; //出版年
 
   Book(String title, String author, int year){
-this.title=title;
-this.author = author;
-this.year=year;
+this.title=title;　//タイトルを設定
+this.author = author;　//著者を設定
+this.year=year;　//出版年を設定
   }
 }
 
